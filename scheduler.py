9fb383ec36d7f17a90511a6c6e90b1aaba767d01
rawatkanak <141995889+rawatkanak@users.noreply.github.com>
2026-01-14 11:11:00 +0530

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -6,6 +6,7 @@ def sort_appointments_fcfs(appointments):
     """
     appointments: list of tuples returned by database.get_appointments
     Each tuple: (appointment_id, appointment_no, patient_name, age, doctor_name, specialization, start_time, end_time, doctor_id)
-    Sorting is already performed in the DB query, but function provided to re-sort if needed.
+    Sorting is already performed  in the DB query, but function provided to re-sort if needed.
     """
     return sorted(appointments, key=lambda x: (x[4], x[1]))  # sort by doctor_name then appointment_no
+
